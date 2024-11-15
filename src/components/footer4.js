64968fb56387a8aa5b549c87c4f39c4009235e7c
Max Cliff <lubalecliff@gmail.com>
2024-11-15 16:39:16 +0300

    updated footer layout

--- src/components/footer4.js
+++ src/components/footer4.js
@@ -1,72 +1,59 @@
-import React, { Fragment } from 'react'
-
-import PropTypes from 'prop-types'
-
+import React from 'react'
+import { Link } from 'react-router-dom' // Import Link
 import './footer4.css'
 
-const Footer4 = (props) => {
+const Footer4 = () => {
+  const handleScrollToTop = () => {
+    window.scrollTo(0, 0); // Scroll to the top when a link is clicked
+  };
+
   return (
     <footer className="footer4-footer7 thq-section-padding">
       <div className="footer4-max-width thq-section-max-width">
         <div className="footer4-content">
           <div className="footer4-logo1">
             <img
-              alt={props.logoAlt}
-              src={props.logoSrc}
+              alt="Logo"
+              src="https://cdn.builder.io/api/v1/image/assets/TEMP/a618eeca7bec3840d12dc2b2400a4228f47af59067ff4be5dc9b77ea7f21b2f0?apiKey=07b7466444764966b3e325d12cdd6280&"
               className="footer4-logo2"
             />
           </div>
           <div className="footer4-links">
-            <a
-              href="/trillionbank"
+            <Link
+              to="/trillionbank"
               className="thq-body-small"
+              onClick={handleScrollToTop}
             >
-              {props.link1 ?? (
-                <Fragment>
-                  <span className="footer4-text16">Bank</span>
-                </Fragment>
-              )}
-            </a>
-            <a
-              href="/trillionit"
+              <span className="footer4-text16">Bank</span>
+            </Link>
+            <Link
+              to="/trillionit"
               className="thq-body-small"
+              onClick={handleScrollToTop}
             >
-              {props.link2 ?? (
-                <Fragment>
-                  <span className="footer4-text18">Digital Wallet</span>
-                </Fragment>
-              )}
-            </a>
-            <a
-              href="/trillipay"
+              <span className="footer4-text18">Digital Wallet</span>
+            </Link>
+            <Link
+              to="/trillipay"
               className="thq-body-small"
+              onClick={handleScrollToTop}
             >
-              {props.link3 ?? (
-                <Fragment>
-                  <span className="footer4-text15">Payment Gateway</span>
-                </Fragment>
-              )}
-            </a>
-            <a
-              href="/trifone"
+              <span className="footer4-text15">Payment Gateway</span>
+            </Link>
+            <Link
+              to="/trifone"
               className="thq-body-small"
+              onClick={handleScrollToTop}
             >
-              {props.link4 ?? (
-                <Fragment>
-                  <span className="footer4-text19">Phone</span>
-                </Fragment>
-              )}
-            </a>
-            <a
-              href="/blokchain"
+              <span className="footer4-text19">Phone</span>
+            </Link>
+            <Link
+              to="/blokchain"
               className="thq-body-small"
+              onClick={handleScrollToTop}
             >
-              {props.link5 ?? (
-                <Fragment>
-                  <span className="footer4-text14">3D Blockchain</span>
-                </Fragment>
-              )}
-            </a>
+              <span className="footer4-text14">3D Blockchain</span>
+            </Link>
           </div>
         </div>
         <div className="footer4-credits">
@@ -74,61 +61,43 @@ const Footer4 = (props) => {
           <div className="footer4-row">
             <div className="footer4-container">
               <span className="thq-body-small">© 2024 TRILLI ON FINANCIAL SUITE</span>
+              
+            </div>
+
+            <div style={{ textAlign: 'center' }}> 
+            <span className="thq-body-small" style={{ color: 'gold' }}>
+              POWERED BY NEO TRILLI AO
+            </span>
+
             </div>
             <div className="footer4-footer-links">
-              <span className="footer4-text11 thq-body-small">
-                {props.privacyLink ?? (
-                  <Fragment>
-                    <span className="footer4-text21">Privacy Policy</span>
-                  </Fragment>
-                )}
-              </span>
-              <span className="thq-body-small">
-                {props.termsLink ?? (
-                  <Fragment>
-                    <span className="footer4-text17">Terms and Conditions</span>
-                  </Fragment>
-                )}
-              </span>
-              <span className="thq-body-small">
-                {props.cookiesLink ?? (
-                  <Fragment>
-                    <span className="footer4-text20">Cookies Policy</span>
-                  </Fragment>
-                )}
-              </span>
+              <Link
+                to="/privacy-policy"
+                className="thq-body-small"
+                onClick={handleScrollToTop}
+              >
+                <span className="footer4-text21">Privacy Policy</span>
+              </Link>
+              <Link
+                to="/terms-and-conditions"
+                className="thq-body-small"
+                onClick={handleScrollToTop}
+              >
+                <span className="footer4-text17">Terms and Conditions</span>
+              </Link>
+              <Link
+                to="/cookies-policy"
+                className="thq-body-small"
+                onClick={handleScrollToTop}
+              >
+                <span className="footer4-text20">Cookies Policy</span>
+              </Link>
             </div>
           </div>
         </div>
       </div>
     </footer>
   )
-}
-
-Footer4.defaultProps = {
-  link5: undefined,
-  link3: undefined,
-  link1: undefined,
-  termsLink: undefined,
-  link2: undefined,
-  link4: undefined,
-  logoAlt: 'https://cdn.builder.io/api/v1/image/assets/TEMP/a618eeca7bec3840d12dc2b2400a4228f47af59067ff4be5dc9b77ea7f21b2f0?apiKey=07b7466444764966b3e325d12cdd6280&',
-  cookiesLink: undefined,
-  logoSrc: "https://cdn.builder.io/api/v1/image/assets/TEMP/a618eeca7bec3840d12dc2b2400a4228f47af59067ff4be5dc9b77ea7f21b2f0?apiKey=07b7466444764966b3e325d12cdd6280&",
-  privacyLink: undefined,
-}
-
-Footer4.propTypes = {
-  link5: PropTypes.element,
-  link3: PropTypes.element,
-  link1: PropTypes.element,
-  termsLink: PropTypes.element,
-  link2: PropTypes.element,
-  link4: PropTypes.element,
-  logoAlt: PropTypes.string,
-  cookiesLink: PropTypes.element,
-  logoSrc: PropTypes.string,
-  privacyLink: PropTypes.element,
 }
 
 export default Footer4
